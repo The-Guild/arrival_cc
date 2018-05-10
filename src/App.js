3c89aea5ebaf7f94b9458a7aea07ddd20f1a9a09
ridgeO <ridge@platypusindustries.com>
2018-05-10 08:52:51 -0700

    add styled-components package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  width: 100vw;
+  min-height: calc(178vw - 84px);
+  background-color: #212121;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppContainer>
+      </AppContainer>
     );
   }
 }
