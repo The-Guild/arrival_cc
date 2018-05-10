f9d97036d75acd3b10f168f90e2f7c0bdc453e28
ridgeO <ridge@platypusindustries.com>
2018-05-10 08:56:02 -0700

    link fonts and change app title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ const AppContainer = styled.div`
   width: 100vw;
   min-height: calc(178vw - 84px);
   background-color: #212121;
+  font-family: 'Cardo', serif;
+  font-weight: 200;
+  letter-spacing: 1px;
+  font-feature-settings: "liga" 0;
 `;
 
 class App extends Component {
